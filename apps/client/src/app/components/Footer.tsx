64930ef31bb7b002fb8a556db02abe5ae96131fe
Yaroslav Dubinskyi <slavabilaz@gmail.com>
2020-09-13 23:54:57 +0200

    add basic layout

---
+++ apps/client/src/app/components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+
+const Footer = () => {
+  return (
+    <Box component="footer" p={6}>
+      <Typography variant="body2" align="center">My Dynasty@2020</Typography>
+    </Box>
+  );
+};
+
+export default Footer;
---
+++ apps/client/src/app/components/Layout.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Header from './Header';
+import Footer from './Footer';
+
+export const Layout = () => {
+  return (
+    <>
+      <Header />
+      <main></main>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;

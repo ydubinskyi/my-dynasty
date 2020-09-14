e99ec3b32cbff1944ee1f6d7501af12b22eaad07
Yaroslav Dubinskyi <slavabilaz@gmail.com>
2020-09-14 10:34:48 +0200

    add routing and empty home page

--- apps/client/src/app/components/Layout.tsx
+++ apps/client/src/app/components/Layout.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 import Header from './Header';
 import Footer from './Footer';
+import Home from '../pages/Home';
 
 export const Layout = () => {
   return (
     <>
       <Header />
-      <main></main>
+      <main>
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </main>
       <Footer />
     </>
   );
